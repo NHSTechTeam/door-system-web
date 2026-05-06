259eb0d133c4c6b90c77b3b257a4aaf199d96f69
Andrew Smiley <76449307+Pinball3D@users.noreply.github.com>
2026-05-06 19:54:28 -0400

    moving more to compose

--- web/db.php
+++ web/db.php
@@ -1,7 +1,13 @@
 <?php
+
+$db_host = getenv('DB_HOST');
+$db_name = getenv('DB_NAME');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
+
 try {
     
-    $db = new PDO("mysql:host=192.168.1.21;dbname=door_dev;charset=utf8mb4", 'root', 'Superce11ular!'); // MAKE SURE TO USE PROD IN PRODUCTION.
+    $db = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass); // MAKE SURE TO USE PROD IN PRODUCTION.
     //echo "Database connection established.";
     //echo  __DIR__ . '/dev.db';
     //$db->exec("PRAGMA foreign_keys = ON;");
