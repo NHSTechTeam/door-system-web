638a02dad907cfd7d9dae44b28155aeec383ea56
Andrew Smiley <76449307+Pinball3D@users.noreply.github.com>
2026-05-22 12:56:34 -0400

    Readd kiosk mode, allow annon in mqtt conf, remove u/p

--- web/kiosk/index.js
+++ web/kiosk/index.js
@@ -3,13 +3,11 @@ statusMessage = document.getElementById("status-message");
 originalMessage = messageBox.innerText;
 
 //make sure we are actually getting these passed through
-//console.log(mqtt_url);
-//console.log(mqtt_username);
-//console.log(mqtt_password);
+console.log(mqtt_url);
 
 
 //WARNING: These variable should be set in index.php and should not be written here or there. They are loaded from env vars in docker
-const client = mqtt.connect(mqtt_url)//, {username: mqtt_username, password: mqtt_password});
+const client = mqtt.connect(mqtt_url)
 
 client.on("connect", () => {
   console.log("connected");
