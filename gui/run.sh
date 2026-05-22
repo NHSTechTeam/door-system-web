3303c64f5dceb450b66b1a9044d100e5438d71ea
Andrew Smiley <76449307+Pinball3D@users.noreply.github.com>
2026-05-22 12:07:14 -0400

    add some more GUI stuff

---
+++ gui/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+apt update
+
+DEBIAN_FRONTEND=noninteractive apt install -y \
+    chromium \
+    openbox \
+    x11-xserver-utils
+
+# Disable screen blanking/screensaver
+xset s off
+xset -dpms
+xset s noblank
+
+# Start lightweight window manager
+openbox &
+
+sleep 2
+
+# Launch Chromium in kiosk mode
+chromium \
+    --no-sandbox \
+    --disable-gpu \
+    --kiosk \
+    https://google.com
